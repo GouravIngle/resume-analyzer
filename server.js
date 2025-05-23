5f684fd578c84b8333e23446a5d7940243b167f1
GouravIngle <gouravingle1874@gmail.com>
2025-05-23 12:43:24 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") }); 
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") }); 
 
-const { analyzeResume } = require("./groq");
+const { analyzeResume } = require("./server/groq");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.post("/analyze", async (req, res) => {
   const { resumeText } = req.body;
